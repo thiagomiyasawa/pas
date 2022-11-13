e231ccce7ddf1f433b75feb5a7d0307a977f52cc
Victor Errera <victor.errera@gmail.com>
2022-11-13 17:59:48 -0300

    victor 13/11

--- Espinhos.cpp
+++ Espinhos.cpp
@@ -3,6 +3,7 @@
 Espinhos::Espinhos(float X, float Y, float l, float a, int id) :
 	Obstaculo(X, Y, l, a, id)
 {
+	body->setFillColor(Color::Cyan);
 }
 
 Espinhos::~Espinhos()
--- PlataformaFalsa.cpp
+++ PlataformaFalsa.cpp
@@ -3,6 +3,7 @@
 PlataformaFalsa::PlataformaFalsa(float X, float Y, float l, float a, int id) :
 	Obstaculo(X, Y, l, a, id)
 {
+	body->setFillColor(Color::Green);
 }
 
 PlataformaFalsa::~PlataformaFalsa()
